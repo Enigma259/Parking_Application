b7d8b5fae66e05322954e0e7917fe2779fc2d9c6
Henrik Jacobsen <henrik259@hotmail.com>
2018-12-04 11:48:33 +0100

    - added documentation to the class "Location".

--- Server/Model/Location.cs
+++ Server/Model/Location.cs
@@ -6,12 +6,18 @@ using System.Device.Location;
 
 namespace Server.Model
 {
+    /// <summary>
+    /// This is the class Location.
+    /// </summary>
     public class Location
     {
         private double longtitude;
         private double latitude;
         private double altitude;
 
+        /// <summary>
+        /// This is the constructor for the class Location.
+        /// </summary>
         public Location()
         {
             string result = GetLocation();
@@ -24,26 +30,64 @@ namespace Server.Model
             }
         }
 
+        /// <summary>
+        /// This method returns the value of the instance longtitude.
+        /// </summary>
+        /// <returns>longtitude</returns>
         public double GetLongtitude()
         {
             return longtitude;
         }
 
+        /// <summary>
+        /// This method returns the value of the instance latitude.
+        /// </summary>
+        /// <returns>latitude</returns>
         public double GetLatitude()
         {
             return latitude;
         }
 
+        /// <summary>
+        /// This method returns the value of the instance altitude.
+        /// </summary>
+        /// <returns>altitude</returns>
+        public double GetAltitude()
+        {
+            return altitude;
+        }
+
+        /// <summary>
+        /// This method changes the value of the instance longtitude.
+        /// </summary>
+        /// <param name="longtitude"></param>
         public void SetLongtitude(double longtitude)
         {
             this.longtitude = longtitude;
         }
 
+        /// <summary>
+        /// This method changes the value of the instance latitude.
+        /// </summary>
+        /// <param name="latitude"></param>
         public void SetLatitude(double latitude)
         {
             this.latitude = latitude;
         }
 
+        /// <summary>
+        /// This method changes the value of the instance altitude.
+        /// </summary>
+        /// <param name="altitude"></param>
+        public void SetAltitude(double altitude)
+        {
+            this.altitude = altitude;
+        }
+
+        /// <summary>
+        /// This method get the current location.
+        /// </summary>
+        /// <returns></returns>
         public string GetLocation()
         {
             string result;
@@ -54,6 +98,8 @@ namespace Server.Model
             {
                 SetLongtitude(coordinates.Longitude);
                 SetLatitude(coordinates.Latitude);
+                SetAltitude(coordinates.Altitude);
+
                 result = "Done";
             }
 
