138743afa8cfe3819084ee6629f71641c163495a
Henrik Jacobsen <henrik259@hotmail.com>
2018-12-04 12:24:31 +0100

    - added documentation to the class "Commercial".

--- Server/Model/Commercial.cs
+++ Server/Model/Commercial.cs
@@ -5,6 +5,9 @@ using System.Web;
 
 namespace Server.Model
 {
+    /// <summary>
+    /// This is the class Commercial.
+    /// </summary>
     public class Commercial
     {
         private static volatile Commercial _instance;
@@ -15,17 +18,19 @@ namespace Server.Model
         /// <summary>
         /// This is the constructor for the class Commercial.
         /// </summary>
-        private Commercial()
+        /// <param name="update_sequence_seconds"></param>
+        private Commercial(int update_sequence_seconds)
         {
             this.last_updated = DateTime.Now;
-            this.update_sequence_seconds = 120;
+            this.update_sequence_seconds = update_sequence_seconds;
         }
 
         /// <summary>
         /// This is a multi threaded singleton for the class Commercial.
         /// </summary>
+        /// <param name="update_sequence_seconds"></param>
         /// <returns>_instance</returns>
-        public static Commercial GetInstance()
+        public static Commercial GetInstance(int update_sequence_seconds)
         {
             if (_instance == null)
             {
@@ -33,7 +38,7 @@ namespace Server.Model
                 {
                     if (_instance == null)
                     {
-                        _instance = new Commercial();
+                        _instance = new Commercial(update_sequence_seconds);
                     }
                 }
             }
@@ -41,26 +46,45 @@ namespace Server.Model
             return _instance;
         }
 
+        /// <summary>
+        /// This method returns the value of the instance last_updated.
+        /// </summary>
+        /// <returns>last_updated</returns>
         public DateTime GetLastUpdated()
         {
             return last_updated;
         }
 
+        /// <summary>
+        /// This method returns the value of the instance update_sequence_seconds.
+        /// </summary>
+        /// <returns>update_sequence_seconds</returns>
         public int GetUpdateSequenceSeconds()
         {
             return update_sequence_seconds;
         }
 
+        /// <summary>
+        /// This method changes the value of the instance last_updated.
+        /// </summary>
         public void SetLastUpdated()
         {
             this.last_updated = DateTime.Now;
         }
 
+        /// <summary>
+        /// This method changes the value of the instance update_sequence_seconds.
+        /// </summary>
+        /// <param name="update_sequence_seconds"></param>
         public void SetUpdateSequenceSeconds(int update_sequence_seconds)
         {
             this.update_sequence_seconds = update_sequence_seconds;
         }
 
+        /// <summary>
+        /// This method checks if it is time to update the commercial.
+        /// </summary>
+        /// <returns>result</returns>
         public bool TimeToUpdate()
         {
             DateTime current_time;
