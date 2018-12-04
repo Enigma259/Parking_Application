4f45dfd870ffcd8dd6748354f487ab4dc26e550f
Henrik Jacobsen <henrik259@hotmail.com>
2018-12-04 13:22:09 +0100

    - Added a new layer called "Database". - added a Ling To SQL classes dbml in the layer "Database" called "SQL_Database" and added tables to it. - added a new class called "Parking" in the layer "Model".

---
+++ Server/Model/Parking.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Server.Database;
+
+namespace Server.Model
+{
+    public class Parking
+    {
+        private static volatile Parking _instance;
+        private static object syncRoot = new object();
+        private SQL_DatabaseDataContext db;
+
+
+        /// <summary>
+        /// This is the constructor for the class Parking.
+        /// </summary>
+        private Parking()
+        {
+            this.db = new SQL_DatabaseDataContext();
+        }
+
+        /// <summary>
+        /// This is a multi threaded singleton for the class Parking.
+        /// </summary>
+        /// <returns>_instance</returns>
+        public static Parking GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (syncRoot)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Parking();
+                    }
+                }
+            }
+
+            return _instance;
+        }
+
+        private void TestData()
+        {
+
+        }
+}
