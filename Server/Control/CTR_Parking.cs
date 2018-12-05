caab360083b32c31ffaf7117bb1101e0c7399296
Henrik Jacobsen <henrik259@hotmail.com>
2018-12-05 19:44:20 +0100

    - added documentation to the class "CTR_Parking."

--- Server/Control/CTR_Parking.cs
+++ Server/Control/CTR_Parking.cs
@@ -7,54 +7,98 @@ using Server.Database;
 
 namespace Server.Control
 {
-    //
+    /// <summary>
+    /// This is the class CTR_Parking.
+    /// </summary>
     public class CTR_Parking
     {
         private Parking parking;
 
-
+        /// <summary>
+        /// This is the constructor for the class CTR_Parking.
+        /// </summary>
         public CTR_Parking()
         {
             this.parking = Parking.GetInstance();
         }
 
-        //
+        /// <summary>
+        /// This method creates a parking place.
+        /// </summary>
+        /// <param name="longtitude"></param>
+        /// <param name="latitude"></param>
+        /// <param name="altitude"></param>
+        /// <param name="parking_name"></param>
+        /// <param name="spaces"></param>
+        /// <param name="vacant"></param>
+        /// <param name="city"></param>
+        /// <param name="country"></param>
+        /// <returns></returns>
         public string Create(double longtitude, double latitude, double altitude, string parking_name, int spaces, int vacant, string city, string country)
         {
             return parking.CreateParking(longtitude, latitude, altitude, parking_name, spaces, vacant, city, country);
         }
-
-        //        
+        
+        /// <summary>
+        /// This method lists all the parking places.
+        /// </summary>
+        /// <returns></returns>
         public List<TableParkingPlace> ListAll()
         {
             return parking.ListAllParkings();
         }
 
-        //
+        /// <summary>
+        /// This method finds a parking place by its id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public TableParkingPlace FindById(int id)
         {
             return parking.FindParkingByid(id);
         }
 
-        //
+        /// <summary>
+        /// This method finds a list of parking places by its longtitude, latitude, and altitude.
+        /// </summary>
+        /// <param name="longtitude"></param>
+        /// <param name="latitude"></param>
+        /// <param name="altitude"></param>
+        /// <returns></returns>
         public List<TableParkingPlace> FindByLocation(double longtitude, double latitude, double altitude)
         {
             return parking.FindParkingByLocation(longtitude, latitude, altitude);
         }
 
-        //
+        /// <summary>
+        ///  This method finds the nearest parking place from a given location.
+        /// </summary>
+        /// <param name="city"></param>
+        /// <param name="country"></param>
+        /// <param name="longtitude"></param>
+        /// <param name="latitude"></param>
+        /// <returns></returns>
         public TableParkingPlace FindNearest(string city, string country, double longtitude, double latitude)
         {
             return parking.FindNearestParking(city, country, longtitude, latitude);
         }
 
-        //
+        /// <summary>
+        /// This method finds a list of parking places by its parking_name.
+        /// </summary>
+        /// <param name="parking_name"></param>
+        /// <returns></returns>
         public List<TableParkingPlace> FindByName(string parking_name)
         {
             return parking.FindParkingByParkingName(parking_name);
         }
 
-        //
+        /// <summary>
+        /// This method finds a list of parking places by its spaces.
+        /// </summary>
+        /// <param name="spaces"></param>
+        /// <param name="where"></param>
+        /// <returns></returns>
         public List<TableParkingPlace> FindBySpaces(int spaces, string where)
         {
             List<TableParkingPlace> result;
@@ -82,7 +126,12 @@ namespace Server.Control
             return result;
         }
 
-        //
+        /// <summary>
+        /// This method finds a list of parking places by its vacant.
+        /// </summary>
+        /// <param name="vacant"></param>
+        /// <param name="where"></param>
+        /// <returns></returns>
         public List<TableParkingPlace> FindByVacant(int vacant, string where)
         {
             List<TableParkingPlace> result;
@@ -110,25 +159,49 @@ namespace Server.Control
             return result;
         }
 
-        //
+        /// <summary>
+        /// This method finds a list of parking places by its city.
+        /// </summary>
+        /// <param name="city"></param>
+        /// <returns></returns>
         public List<TableParkingPlace> FindByCity(string city)
         {
             return parking.FindParkingByCity(city);
         }
 
-        //
+        /// <summary>
+        /// This method finds a list of parking places by its country.
+        /// </summary>
+        /// <param name="country"></param>
+        /// <returns></returns>
         public List<TableParkingPlace> FindByCountry(string country)
         {
             return parking.FindParkingByCountry(country);
         }
 
-        //
+        /// <summary>
+        /// This method updates a parking place.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="longtitude"></param>
+        /// <param name="latitude"></param>
+        /// <param name="altitude"></param>
+        /// <param name="parking_name"></param>
+        /// <param name="spaces"></param>
+        /// <param name="vacant"></param>
+        /// <param name="city"></param>
+        /// <param name="country"></param>
+        /// <returns></returns>
         public string Update(int id, double longtitude, double latitude, double altitude, string parking_name, int spaces, int vacant, string city, string country)
         {
             return parking.UpdateParking(id, longtitude, latitude, altitude, parking_name, spaces, vacant, city, country);
         }
 
-        //
+        /// <summary>
+        /// This method deletes a parking place.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public string Delete(int id)
         {
             return parking.DeleteParking(id);
