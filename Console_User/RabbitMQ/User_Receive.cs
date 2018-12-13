e8f7c1eab2aa94664148a5ff90c23f89ddfc2198
Henrik Jacobsen <henrik259@hotmail.com>
2018-12-13 11:09:22 +0100

    - added singleton to the class "User_Receive".

--- Console_User/RabbitMQ/User_Receive.cs
+++ Console_User/RabbitMQ/User_Receive.cs
@@ -8,6 +8,35 @@ namespace Console_User.RabbitMQ
 {
     public sealed class User_Receive
     {
+        private static volatile User_Receive _instance;
+        private static readonly object syncRoot = new object();
 
+        /// <summary>
+        /// This is the constructor for the class User_Receive.
+        /// </summary>
+        private User_Receive()
+        {
+
+        }
+
+        /// <summary>
+        /// This is a multi threaded singleton for the class User_Receive.
+        /// </summary>
+        /// <returns>_instance</returns>
+        public static User_Receive GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (syncRoot)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new User_Receive();
+                    }
+                }
+            }
+
+            return _instance;
+        }
     }
 }
