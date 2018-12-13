d6f3293d6a7a2af35e79ffee65e04c76c3c7fb31
Henrik Jacobsen <henrik259@hotmail.com>
2018-12-13 08:06:35 +0100

    - Added 4 classes to the Control layer

---
+++ Console_Server/Control/CTR_Location.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Server.Model;
+
+namespace Server.Control
+{
+    /// <summary>
+    /// this is the class CTR_Location.
+    /// </summary>
+    public class CTR_Location
+    {
+        private static volatile CTR_Location _instance;
+        private static object syncRoot = new object();
+
+        private Location location;
+
+        /// <summary>
+        /// This is the constructor for the class CTR_Location.
+        /// </summary>
+        private CTR_Location()
+        {
+            this.location = new Location();
+        }
+
+        /// <summary>
+        /// This is a multi threaded singleton for the class CTR_Location.
+        /// </summary>
+        /// <returns>_instance</returns>
+        public static CTR_Location GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (syncRoot)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new CTR_Location();
+                    }
+                }
+            }
+
+            return _instance;
+        }
+
+
+        public double GetLongtitude()
+        {
+            return location.GetLongtitude();
+        }
+
+        public double GetLatitude()
+        {
+            return location.GetLatitude();
+        }
+
+        public double GetAltitude()
+        {
+            return location.GetAltitude();
+        }
+        
+        public string SetLocation()
+        {
+            return location.SetLocation();  
+        }
+    }
+}
