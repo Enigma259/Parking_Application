46adeb29dfcd2b2da0c011bcaaad27afdc8a7be9
Henrik Jacobsen <henrik259@hotmail.com>
2019-01-11 22:28:03 +0100

    - adds documentation to the class "Program".

--- Console_Server/Program.cs
+++ Console_Server/Program.cs
@@ -7,10 +7,17 @@ using Console_Server.RabbitMQ;
 
 namespace Console_Server
 {
+    /// <summary>
+    /// This is the class Program.
+    /// </summary>
     class Program
     {
         private static Server_Receive receive_message;
 
+        /// <summary>
+        /// This method is the starting method for the project Console_Server.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             receive_message = new Server_Receive();
