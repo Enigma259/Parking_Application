00b29b1992d9bf3a7c6c13069cbfbba980fe4a84
Henrik Jacobsen <henrik259@hotmail.com>
2019-01-11 22:26:20 +0100

    - adds documentation to the class "Reservation".

--- Console_Server/Model/Reservation.cs
+++ Console_Server/Model/Reservation.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 namespace Console_Server.Model
 {
     /// <summary>
-    /// 
+    /// This is the class Reservation.
     /// </summary>
     public sealed class Reservation
     {
